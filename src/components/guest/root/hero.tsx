d9cf6d0ccc713ab6b6bdc57eb4dd0c625a58eb4e
Muhammad Abdul Haseeb <144768838+AbdulHaseeb-1@users.noreply.github.com>
2024-09-03 23:04:31 +0500

    test

--- src/components/guest/root/hero.tsx
+++ src/components/guest/root/hero.tsx
@@ -1,21 +1,25 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
-import { useEffect } from "react";
 
 export default function Hero(props: any) {
   return (
-    <motion.div
-      className="w-full min-h-screen flex flex-col items-center justify-center p-4 transition-colors duration-200 bg-background text-foreground overflow-hidden"
-    >
-      <div className="max-w-3xl text-center space-y-6 mx-auto">
+    <motion.div className="relative bg-gradient-to-tr  w-full min-h-screen flex flex-col items-center justify-center p-4 transition-colors duration-200 bg-background text-foreground overflow-hidden">
+       <div className="max-w-3xl text-center space-y-6 mx-auto">
         <motion.h1
           initial={{ y: -50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, ease: "easeOut", delay: 0.5 }}
           className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl"
         >
-          Transform Your Life with a Fresh Look
+          <span className=" bg-clip-text text-transparent bg-gradient-to-bl from-green-400 to-emerald-500 drop-shadow-lg ">
+            Transform
+          </span>
+          <span className="">Your Life with a</span>
+          <span className="bg-clip-text text-transparent bg-gradient-to-bl from-green-400 to-emerald-500">
+            {" "}
+            Fresh Look
+          </span>{" "}
         </motion.h1>
         <motion.p
           initial={{ opacity: 0 }}
@@ -30,15 +34,18 @@ export default function Hero(props: any) {
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1, delay: 2 }}
-          className="flex flex-col sm:flex-row justify-center gap-4"
+          className="flex flex-col sm:flex-row justify-center gap-4  "
         >
-          <Button size="lg" className="text-lg w-full sm:w-auto">
+          <Button
+            size="lg"
+            className="z-50 text-lg  sm:w-auto bg-gradient-to-bl from-green-400 to-emerald-500 shadow-2xl  shadow-primary  "
+          >
             View Products
           </Button>
           <Button
             size="lg"
-            variant={"secondary"}
-            className="text-lg w-full sm:w-auto"
+            variant={"outline"}
+            className="text-lg  sm:w-auto border-[#34BE82] hover:bg-[34BE82] hover:shadow-2xl hover:shadow-primary transition-shadow duration-200  "
           >
             Explore Categories
           </Button>
