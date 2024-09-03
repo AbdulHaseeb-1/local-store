3953e4bd853af6182a7abe0ce1499086ee36c2ce
Muhammad Abdul Haseeb <144768838+AbdulHaseeb-1@users.noreply.github.com>
2024-09-03 10:52:10 +0500

    test

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  // baseURL: "http://localhost:3000/api",
-  baseURL: "https://local-store-phi.vercel.app/api",
+  baseURL: "http://localhost:3000/api",
+  // baseURL: "https://local-store-phi.vercel.app/api",
 });
 
 export default axios;
