6a91e07d1f786d721972d75fd7a19c639d714c40
Muhammad Abdul Haseeb <144768838+AbdulHaseeb-1@users.noreply.github.com>
2024-09-07 16:25:12 +0500

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const axios = Axios.create({
   // baseURL: "http://localhost:3000/api",
-  baseURL: "https://local-store-peach.vercel.app/api",
+  baseURL: "https://local-store-kappa.vercel.app/api",
 });
 
 export default axios;
